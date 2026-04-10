8de6b989842c29aff0befc566ccb38f94e22744b
paytonbaldridge777 <paytonbaldridge@gmail.com>
2026-04-10 14:27:21 +0000

    chore: add migration note for deprecated @cloudflare/next-on-pages adapter

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,11 @@
 import type { NextConfig } from "next";
 
+// TODO [ADAPTER-MIGRATION]: @cloudflare/next-on-pages is deprecated as of 2026.
+// Cloudflare recommends migrating to the OpenNext adapter: https://opennext.js.org/cloudflare
+// Build command will change from: npx @cloudflare/next-on-pages@1
+// To: npx opennextjs-cloudflare@latest
+// See DEPLOYMENT.md for current build settings. Address this before next major Next.js upgrade.
+
 const nextConfig: NextConfig = {
   /* config options here */
 };
